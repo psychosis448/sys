fb377ec1cfcfdac8bc60b4d222c999d711ac8282
psychosis448 <>
2025-01-04 16:53:21 +0700

    Add geistmono font

--- system/graphical/fonts.nix
+++ system/graphical/fonts.nix
@@ -6,14 +6,15 @@
   fonts = {
     packages = with pkgs; [
       nerd-fonts.geist-mono
+      nerd-fonts.symbols-only
       noto-fonts-emoji
       tlwg
     ];
     fontconfig = {
       defaultFonts = {
-        # serif = ["Geist Nerd Font Propo"];
-        # sansSerif = ["Geist Nerd Font Propo"];
-        # monospace = ["Geist Nerd Font Mono"];
+        sansSerif = ["GeistMono Nerd Font Propo"];
+        serif = ["GeistMono Nerd Font Propo"];
+        monospace = ["GeistMono Nerd Font Mono"];
         emoji = ["Noto Color Emoji"];
       };
     };
