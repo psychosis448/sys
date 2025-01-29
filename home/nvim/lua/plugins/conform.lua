75aafc6935d97e0cc60cac5bbecf1194ee7ca9a1
psychosis448 <psychosis448@protonmail.com>
2025-01-29 20:05:52 +0100

    Add nvim config for blink, conform and lsp (#2)
    Adds config for tab completion with blink, formatting with conform, nixd as lsp and go for programming.
    
    ---------
    
    Co-authored-by: psychosis448 <>

--- home/nvim/lua/plugins/conform.lua
+++ home/nvim/lua/plugins/conform.lua
@@ -2,7 +2,10 @@ return {
   "stevearc/conform.nvim",
   optional = true,
   opts = {
+    event = { "BufWritePre" },
     formatters_by_ft = {
+      go = { "goimports", "gofumpt" },
+      nix = { "alejandra" },
       ["markdown"] = { "prettierd", "markdownlint-cli2", "markdown-toc" },
       ["markdown.mdx"] = { "prettierd", "markdownlint-cli2", "markdown-toc" },
     },
